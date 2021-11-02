61cd883bd1e5338c615b3f43900aa515708a520b
ijichi <crazy_soci80@yahoo.co.jp>
2021-11-02 20:35:05 +0900

    index作成

---
+++ class_based_view/store/models.py
@@ -0,0 +1,17 @@
+from abc import abstractclassmethod
+from django.db import models
+
+class BaseModel(models.Model):
+    create_at = models.DateTimeField()
+    update_at = models.DateTimeField()
+
+    class Meta:
+        abstract = True
+
+class Books(BaseModel):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+
+    class Meta:
+        db_table='books'
+        
